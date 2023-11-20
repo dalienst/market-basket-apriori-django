cd655aa2107edb14b62ece6ddbe55434e162e205
dalienst <oduordalienst@gmail.com>
2023-11-20 12:13:35 +0300

    add apriori analysis

--- recommendations/logic.py
+++ recommendations/logic.py
@@ -5,6 +5,53 @@ import plotly.express as px
 import numpy as np
 
 
+def perform_apriori_analysis(
+    data_path="https://res.cloudinary.com/devowino/raw/upload/v1700430513/Data%20Mining/Market_Basket_Optimisation_yqk5ci.csv",
+):
+    """
+    performing apriori analysis on a dataset
+    """
+    data = pd.read_csv(data_path)
+
+    # Gathering all items into a list of lists
+    transactions = []
+    for i in range(data.shape[0]):
+        transaction = [item for item in data.iloc[i].dropna()]
+        transactions.append(transaction)
+
+    # Initializing the TransactionEncoder
+    te = TransactionEncoder()
+
+    # Using the TransactionEncoder to fit and transform the transactions
+    te_ary = te.fit(transactions).transform(transactions)
+
+    # Creating a DataFrame with the encoded transactions
+    dataset = pd.DataFrame(te_ary, columns=te.columns_)
+
+    # Applying Apriori algorithm
+    frequent_itemsets = apriori(dataset, min_support=0.01, use_colnames=True)
+    frequent_itemsets["length"] = frequent_itemsets["itemsets"].apply(lambda x: len(x))
+
+    # Filtering the itemsets based on length and support
+    filtered_itemsets = frequent_itemsets[
+        (frequent_itemsets["length"] == 2) & (frequent_itemsets["support"] >= 0.05)
+    ]
+
+    # items with a length of 3
+    filtered_length3 = frequent_itemsets[(frequent_itemsets["length"] == 3)].head(3)
+
+    # Mining association rules
+    # Creating antecedents and consequences
+    rules = association_rules(frequent_itemsets, metric="lift", min_threshold=1.2)
+    rules["antecedents_length"] = rules["antecedents"].apply(lambda x: len(x))
+    rules["consequents_length"] = rules["consequents"].apply(lambda x: len(x))
+
+    # Sort values based on confidence
+    sorted_rules = rules.sort_values("confidence", ascending=False)
+
+    return frequent_itemsets, filtered_itemsets, filtered_length3, sorted_rules
+
+
 def get_recommendations(input_data):
     data = pd.read_csv(
         "https://res.cloudinary.com/devowino/raw/upload/v1700430513/Data%20Mining/Market_Basket_Optimisation_yqk5ci.csv"
@@ -17,7 +64,7 @@ def get_recommendations(input_data):
     te_ary = te.fit(transactions).transform(transactions)
     dataset = pd.DataFrame(te_ary, columns=te.columns_)
 
-    frequent_itemsets = apriori(dataset, min_support=0.01, use_colnames=True)
+    frequent_itemsets = apriori(dataset, min_support=0.05, use_colnames=True)
     rules = association_rules(frequent_itemsets, metric="lift", min_threshold=1.2)
 
     # For simplicity, just return the first 5 rules as recommendations
--- recommendations/urls.py
+++ recommendations/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import recommend, render_visualizations
+from .views import recommend, render_visualizations, apriori_results
 
 app_name = "recommendations"
 
 urlpatterns = [
     path("recommend/", recommend, name="recommend"),
     path("visualise/", render_visualizations, name="visualise"),
+    path("apriori-results/", apriori_results, name="aprioriResults"),
 ]
